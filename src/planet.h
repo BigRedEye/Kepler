e6ca90109fb414d977376ae5d70cce5b9ecaf99b
BigRedEye <skvor.sm@gmail.com>
2016-10-12 00:32:04 +0300

    Add planets options

--- src/planet.h
+++ src/planet.h
@@ -2,12 +2,24 @@
 #define PLANET_H
 
 #include <QColor>
+#include <QString>
+#include <algorithm>
 
 class Planet
 {
 public:
-    Planet(long double x, long double y, long double vx, long double vy, long double r, long double m, QColor color):x(x),y(y),vx(vx),vy(vy),r(r),m(m),color(color){
+    Planet(long double x, long double y, long double vx, long double vy, long double r, long double m, QColor color = QColor(rand()%256,rand()%256,rand()%256)):x(x),y(y),vx(vx),vy(vy),r(r),m(m),color(color){
         ax = ay = 0;
+        
+        if (m >= 100000.)
+            name = "Black Hole";
+        else if (m >= 1000.)
+            name = "Sun";
+        else if (m >= 1.)
+            name = "Planet";
+        else
+            name = "Moon";
+        
     }
     void addForce(long double fx,long double fy);
     void addForce(const Planet &other);
@@ -18,7 +30,8 @@ public:
     long double x,y;//real coords
     long double r,m;//radius and mass
     long double ax,ay;
-    long double vx,vy;//real velocity    
+    long double vx,vy;//real velocity  
+    QString name;
 };
 
 
--- src/widget.h
+++ src/widget.h
@@ -4,8 +4,14 @@
 #include <QWidget>
 #include <QTimer>
 #include <QGraphicsScene>
+#include <QMouseEvent>
+#include <QLineEdit>
+#include <QVector>
+
 #include <vector>
+
 #include "planet.h"
+
 
 namespace Ui {
 class Widget;
@@ -24,14 +30,39 @@ public:
     long double camX0,camY0,camX1,camY1;
     
     explicit Widget(QWidget *parent = 0);
+    
     ~Widget();
+    
     void updatePhys(long double dt);
     
+    void updatePlanetInfo();
+    
+    void updatePlanetsList();
+    
+    void changePlanetInfo();
+    
+    void setPlanetInfo();
+    
 private:
+    bool editing = 0;
+    
     Ui::Widget *ui;
     
+    bool isPlaying = 1;
+    
+    int chosenPlanet = -1;
+    
+    int frameRate = 100;
+    
+    QVector<QLineEdit> planetList;    
 private slots:
-    void update();
+    void update(bool redrawOnly = 0);
+    
+    void on_playButton_clicked();
+    
+    void mousePressEvent(QMouseEvent *event);
+    void on_pushButton_clicked();
+    void on_pushButton_2_clicked();
 };
 
 #endif // WIDGET_H
